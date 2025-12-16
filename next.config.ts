2204aed31deedf36db8fe66d7cab8aa33bd9f409
Bouchra El ouahabi Jabbah <ebouchra163@gmail.com>
2025-12-16 18:18:19 +0000

    "Vull preparar la web per pujar-la a Netlify via GitHub de manera dinàmi

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,27 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
     ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'esypro.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'encrypted-tbn0.gstatic.com',
+      },
+    ],
   },
 };
 
