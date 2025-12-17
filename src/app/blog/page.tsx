2b867adb18a330d6adb8c1ffdf744cce64b4d4b3
Bouchra El ouahabi Jabbah <ebouchra163@gmail.com>
2025-12-17 15:42:08 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -4,8 +4,8 @@ import { posts } from '@/lib/blog-posts';
 import { PlaceHolderImages } from '@/lib/placeholder-images';
 import BlogList from './blog-list';
 
-export default function BlogPage() {
-  // Aquest és un Server Component, obté les dades i les passa
-  // al Client Component responsable de la renderització.
+export default async function BlogPage() {
+  // Este es un Server Component, obtiene los datos y los pasa
+  // al Client Component responsable de la renderización.
   return <BlogList posts={posts} images={PlaceHolderImages} />;
 }
