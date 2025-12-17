82fff17787e8b1856e2b174a4e78c421d5b1a82f
Bouchra El ouahabi Jabbah <ebouchra163@gmail.com>
2025-12-17 15:25:29 +0000

    en la carpeat public he subido una nueva imagen que se llama Logo_Bouchr

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -5,7 +5,7 @@ export function Logo({ className }: { className?: string }) {
   return (
     <div className={cn("flex items-center", className)}>
       <Image
-        src="/logo.png"
+        src="/Logo_Bouchra.png"
         alt="Logo de la empresa"
         width={140}
         height={40}
